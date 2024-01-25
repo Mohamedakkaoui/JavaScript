0495ff0dca010720220983a926b3c5501eac95c3
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-01-25 22:08:50 +0100

    The Basics

---
+++ The-basics.js
@@ -0,0 +1,20 @@
+//Task 1 : Warm up
+let firstname = 'Mohamed'
+let lastname = 'Akkaoui'
+const PI = 3.14
+let radius = 12
+let favoriteSuperhero = 'Black Panther'
+let favoriteQuote = 'Get busy living or get busy dying'
+
+//Task 2
+let fullname = firstname + " " + lastname
+let area = PI * Math.pow(radius, 2)
+let perimetre = 2 * PI * radius
+let motivation = `a wise man name ${favoriteSuperhero} said : ${favoriteQuote}`
+
+//task 3
+let a = 3;
+let b = 10;
+
+[a, b] = [b, a]
+console.log("After swapping: a = ", a, " and b = ", b)
