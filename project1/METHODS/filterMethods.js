f2cf356bdda1c162f89f124273be02058e11219f
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-02-01 12:00:26 +0100

    filters products based on the specified category.

---
+++ project1/METHODS/filterMethods.js
@@ -0,0 +1,8 @@
+const filterByCategory = (products, category1) =>
+{
+  let filteredproducts = products.filter ((product)=> product.category == category1)
+  return filteredproducts
+}
+module.exports ={
+  filterByCategory
+}
