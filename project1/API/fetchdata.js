fd86416e775d371c02c8518d72659655f6a1f12c
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-02-02 17:09:39 +0100

    function fetchData(url) that takes a URL as a parameter and uses fetch to get data from the FakeStoreAPI

--- project1/API/fetchdata.js
+++ project1/API/fetchdata.js
@@ -1,3 +1,10 @@
+/**
+ * fetchData - Fetches product data from te scecified url
+ * 
+ * @param {string} url - the URL from which to fect the data 
+ * 
+ * @returns -  a promise tat resolves with a array of product data
+ */
 const fetchData = async (url) => {
   try {
     const response = await fetch(url)
