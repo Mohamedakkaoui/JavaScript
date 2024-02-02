c563fb6e2be46d7ef2201b3737e49d1e28d0c2dc
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-02-02 17:11:01 +0100

    function filterByCategory(products, category) that filters products based on the specified category

--- project1/METHODS/filterMethods.js
+++ project1/METHODS/filterMethods.js
@@ -1,3 +1,11 @@
+/**
+ * filterByCategory - filters product by category
+ * 
+ * @param {Array} products - An array of products objects
+ * @param {string} category1 - the category by which to filter the products
+ * 
+ * @returns - An array of products belonging to the specified category
+ */
 const filterByCategory = (products, category1) =>
 {
   let filteredproducts = products.filter ((product)=> product.category === category1)
