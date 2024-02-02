f10c197c58d5530866471e02b1ada9b746ca07e2
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-02-02 17:12:45 +0100

    functions sortByPriceAsc(products) and sortByPriceDesc(products) that sort products by price in ascending and descending order, respectively1

--- project1/METHODS/sortMethods.js
+++ project1/METHODS/sortMethods.js
@@ -1,7 +1,21 @@
+/**
+ * sortByPriceAsc - sorts the product by price in ascending order
+ *  
+ * @param {Array} products - An array of products
+ *  
+ * @returns - an array of sorted products 
+ */
 const sortByPriceAsc =(products) => {
   products.sort((a, b) => a.price - b.price)
   return products
 }
+/**
+ * sortByPriceDesc - sorts the product by price in a descending order
+ *  
+ * @param {Array} products - An array of products
+ *  
+ * @returns - an array of sorted products 
+ */
 const sortByPriceDesc =(products) => {
   products.sort((a,b) => b.price - a.price)
   return products
