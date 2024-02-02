4dffd3a2a4705f11fd9a3c9843887f82e35eb1a7
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-02-02 17:14:44 +0100

    main function where we import all functions from their respective folders and use them to demonstarte the instructions

--- project1/main.js
+++ project1/main.js
@@ -1,17 +1,19 @@
 const {fetchData} = require ('./API/fetchdata')
 const {calculateAveragePrice} = require ('./METHODS/calculationMethods')
 const {filterByCategory} = require ('./METHODS/filterMethods')
-const {detailproduct} = require ('./METHODS/productDetails')
+const {detailProduct} = require ('./METHODS/productDetails')
 const {searchByKeyword} = require ('./METHODS/searchMethods')
 const {sortByPriceAsc,sortByPriceDesc} = require ('./METHODS/sortMethods')
 
-async function prouctserive (){
-  const products = await fetchData("https://fakestoreapi.com/products")
-  // console.log(detailproduct(products, 6))
-  // console.log(filterByCategory(products, "men's clothing"))
-  // console.log(calculateAveragePrice(products))
-  // console.log(sortByPriceAsc(products))
-  // console.log(sortByPriceDesc(products))
+let url = "https://fakestoreapi.com/products"
+
+async function productService (){
+  const products = await fetchData(url)
+  console.log(detailProduct(products, 6))
+  console.log(filterByCategory(products, "men's clothing"))
+  console.log(calculateAveragePrice(products))
+  console.log(sortByPriceAsc(products))
+  console.log(sortByPriceDesc(products))
   console.log(searchByKeyword(products, "Men"))
 }
-prouctserive()
+productService()
