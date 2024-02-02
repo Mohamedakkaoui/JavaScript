7b014828da9187296d62da4721c9da4d6f6df61a
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-02-02 17:12:08 +0100

    function searchByKeyword(products, keyword) that searches for products containing the given keyword in their title or description

--- project1/METHODS/searchMethods.js
+++ project1/METHODS/searchMethods.js
@@ -1,3 +1,11 @@
+/**
+ * searchByKeyword - filters products by category
+ * 
+ * @param {string} products - An array of products objects 
+ * @param {string} keyword - the keyword by which to filter the products
+ * 
+ * @returns - An array of products that has the specified keyword
+ */
 const searchByKeyword = ((products, keyword) => {
   return (products.filter((product) => {
   return  product.title.match(keyword)!== null || product.description.match(keyword)!== null}))
